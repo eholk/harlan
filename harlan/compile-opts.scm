0faf2b8d46fd750f3cd876f2534c2100e46081b1
Claire Alvis <claire.alvis@gmail.com>
2011-10-31 20:06:29 -0400

    Parameter for benchmarking

--- harlan/compile-opts.scm
+++ harlan/compile-opts.scm
@@ -1,39 +1,42 @@
 (library
-    (harlan compile-opts)
+  (harlan compile-opts)
   (export
-   passes
-   verbose
-   verify
-   trace-pass)
+    passes
+    verbose
+    benchmark
+    verify
+    trace-pass)
   (import
-   (rnrs)
-   (util color)
-   (only (chezscheme) pretty-print make-parameter))
-
-  (define verbose (make-parameter #f))
-  (define verify (make-parameter #t))
+    (rnrs)
+    (util color)
+    (only (chezscheme) pretty-print make-parameter))
   
-  (define trace-pass
-    (lambda (m pass expr)
-      (if (verbose)
-          (begin
-            (set-color 'green) (display "Beginning pass ") (display m)
+(define verbose (make-parameter #f))
+(define verify (make-parameter #t))
+(define benchmark (make-parameter #f))
+
+(define trace-pass
+  (lambda (m pass expr)
+    (if (verbose)
+        (begin
+          (newline)
+          (set-color 'green) (display "Beginning pass ") (display m)
+          (set-color 'default) (newline)
+          (let ((expr (pass expr)))
+            (set-color 'green)
+            (display "Pass ") (display m) (display " output:")
             (set-color 'default) (newline)
-            (let ((expr (pass expr)))
-              (set-color 'green)
-              (display "Pass ") (display m) (display " output:")
-              (set-color 'default) (newline)
-              (pretty-print expr) (newline)
-              expr))
-          (pass expr))))
+            (pretty-print expr) (newline)
+            expr))
+        (pass expr))))
 
-  (define-syntax passes
-    (syntax-rules ()
-      ((_ pass-name ...)
-       (lambda (expr)
-         (let* ((expr (trace-pass (symbol->string 'pass-name) pass-name expr))
-                ...)
-           expr)))))
+(define-syntax passes
+  (syntax-rules ()
+    ((_ pass-name ...)
+     (lambda (expr)
+       (let* ((expr (trace-pass (symbol->string 'pass-name) pass-name expr))
+              ...)
+         expr)))))
 
-  ;; end library
-  )
+;; end library
+)
