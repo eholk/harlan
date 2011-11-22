7bea9ae81187e445dfbd41ab9be9d7430e46eccf
Eric Holk <eholk@cs.indiana.edu>
2011-11-21 17:36:45 -0800

    Use elegant-weapons set library in annotate-free-vars.

--- harlan/middle/annotate-free-vars.scm
+++ harlan/middle/annotate-free-vars.scm
@@ -1,7 +1,10 @@
 (library
   (harlan middle annotate-free-vars)
   (export annotate-free-vars)
-  (import (rnrs) (elegant-weapons helpers))
+  (import
+   (rnrs)
+   (elegant-weapons helpers)
+   (elegant-weapons sets))
 
 (define-match annotate-free-vars
   ((module . ,[annotate-decl* -> decl*])
@@ -23,7 +26,7 @@
 
 (define-match annotate-stmt
   ((begin ,[stmt* fv**] ...)
-   (values `(begin . ,stmt*) (apply union* fv**)))
+   (values `(begin . ,stmt*) (apply union fv**)))
   ((kernel (((,x* ,t*)
              (,[annotate-expr -> xs* fv**] ,ts*)) ...)
      ,[stmt fv*])
@@ -31,20 +34,20 @@
      (values
        `(kernel (((,x* ,t*) (,xs* ,ts*)) ...)
           (free-vars . ,fv*) ,stmt)
-       (apply union* fv* fv**))))
+       (apply union fv* fv**))))
   ((return ,[annotate-expr -> e fv*])
    (values `(return ,e) fv*))
   ((if ,[annotate-expr -> test tfv*] ,[conseq cfv*])
    (values `(if ,test ,conseq) (union tfv* cfv*)))
   ((if ,[annotate-expr -> test tfv*] ,[conseq cfv*] ,[alt afv*])
    (values `(if ,test ,conseq ,alt)
-     (union* tfv* cfv* afv*)))
+     (union tfv* cfv* afv*)))
   ((do ,[annotate-expr -> e fv*])
    (values `(do ,e) fv*))
   ((let ((,x* ,[annotate-expr -> e* fv**]) ...) ,[stmt fv*])
    (let ((fv* (fold-right remove fv* x*)))
      (values `(let ((,x* ,e*) ...) ,stmt)
-       (apply union* fv* fv**))))
+       (apply union fv* fv**))))
   ((while ,[annotate-expr -> e efv*] ,[stmt sfv*])
    (values `(while ,e ,stmt) (union efv* sfv*)))
   ((for (,x ,[annotate-expr -> start sfv*]
@@ -52,7 +55,7 @@
      ,[stmt bfv*])
    (let ((bfv* (remq x bfv*)))
      (values `(for (,x ,start ,end) ,stmt)
-       (union* bfv* sfv* efv*))))
+       (union bfv* sfv* efv*))))
   ((set! ,[annotate-expr -> x xfv*] ,[annotate-expr -> e efv*])
    (values `(set! ,x ,e) (union xfv* efv*)))
   ((print ,[annotate-expr -> e fv*])
@@ -71,14 +74,14 @@
   ((call ,[rator fv*] ,[rand* fv**] ...)
    (values
      `(call ,rator . ,rand*)
-     (apply union* fv* fv**)))
+     (apply union fv* fv**)))
   ((let ((,x* ,[e* fv**]) ...) ,[expr fv*])
    (let ((fv* (fold-right remove fv* x*)))
      (values `(let ((,x* ,e*) ...) ,expr)
-       (apply union* fv* fv**))))
+       (apply union fv* fv**))))
   ((if ,[test tfv*] ,[conseq cfv*] ,[alt afv*])
    (values `(if ,test ,conseq ,alt)
-     (union* tfv* cfv* afv*)))
+     (union tfv* cfv* afv*)))
   ((sizeof ,t) (values `(sizeof ,t) '()))
   ((addressof ,[e fv*]) (values `(addressof ,e) fv*))
   ((,op ,[lhs lfv*] ,[rhs rfv*])
@@ -86,25 +89,6 @@
    (values `(,op ,lhs ,rhs) (union lfv* rfv*)))
   ((vector-ref ,t ,[v vfv*] ,[i ifv*])
    (values `(vector-ref ,t ,v ,i) (union vfv* ifv*))))
-
-(define union
-  (lambda (s1 s2)
-    (cond
-      [(null? s1) s2]
-      [(member (car s1) s2) (union (cdr s1) s2)]
-      [else (cons (car s1) (union (cdr s1) s2))])))
-
-(define union*
-  (lambda args
-    (union*-aux args)))
-
-(define union*-aux
-  (lambda (args)
-    (cond
-      [(null? args) '()]
-      [(null? (cdr args)) (car args)]
-      [else (union (union (car args) (cadr args))
-                   (union*-aux (cddr args)))])))
 
 ;; end library
 )
