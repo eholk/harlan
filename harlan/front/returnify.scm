e1f36c7ffcc9a9fe60b33830df7053ff9a550e73
Claire Alvis <claire.alvis@gmail.com>
2011-12-12 21:16:27 -0500

    Moved returnify to before the typechecker

---
+++ harlan/front/returnify.scm
@@ -0,0 +1,32 @@
+(library
+  (harlan front returnify)
+  (export returnify)
+  (import (rnrs) (elegant-weapons helpers))
+  
+(define-match returnify
+  ((module ,[returnify-decl -> decl*] ...)
+   `(module . ,decl*)))
+
+(define-match returnify-decl
+  ((fn ,name ,args ,[returnify-stmt -> stmt])
+   `(fn ,name ,args ,stmt))
+  ((extern ,name ,args -> ,rtype)
+   `(extern ,name ,args -> ,rtype)))
+
+(define-match returnify-stmt
+  ((var ,x) `(return (var ,x)))       
+  ((let ((,x ,e) ...) ,[stmt])
+   `(let ((,x ,e) ...) ,stmt))
+  ((print ,e) `(begin (print ,e) (return (void))))
+  ((assert ,e) `(begin (assert ,e) (return (void))))
+  ((return ,expr)
+   `(return ,expr))
+  ((do ,expr) `(return ,expr))
+  ((begin ,stmt* ... ,[returnify-stmt -> stmt])
+   `(begin ,@stmt* ,stmt))
+  ((if ,test ,[conseq])
+   `(if ,test ,conseq))
+  ((if ,test ,[conseq] ,[alt])
+   `(if ,test ,conseq ,alt)))
+
+)
