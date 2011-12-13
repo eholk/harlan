5219e701296485507ed39ef5df97d494829847f5
Claire Alvis <claire.alvis@gmail.com>
2011-12-12 21:18:56 -0500

    Added returnify to the frontend

--- harlan/front/compile-front.scm
+++ harlan/front/compile-front.scm
@@ -6,10 +6,12 @@
     (harlan front parser)
     (harlan front typecheck)
     (harlan front nest-lets)
+    (harlan front returnify)
     (harlan compile-opts)
     (only (harlan verification-passes)
       verify-harlan
       verify-nest-lets
+      verify-returnify
       verify-parse-harlan
       verify-typecheck))
   
@@ -20,6 +22,8 @@
       verify-nest-lets
       parse-harlan
       verify-parse-harlan
+      returnify
+      verify-returnify
       typecheck
       verify-typecheck))
   
