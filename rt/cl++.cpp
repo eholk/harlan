9fb3a20868e5683d5930616c10dee93fff10cfea
Eric Holk <eholk@cs.indiana.edu>
2013-06-18 14:15:28 -0600

    Cleaned up the error formatting code, added support for another error handler.

--- rt/cl++.cpp
+++ rt/cl++.cpp
@@ -328,6 +328,7 @@ string cl::format_status(cl_int e) {
         STATUS_STR(CL_SUCCESS);
         STATUS_STR(CL_BUILD_PROGRAM_FAILURE);
         STATUS_STR(CL_COMPILER_NOT_AVAILABLE);
+        STATUS_STR(CL_DEVICE_NOT_AVAILABLE);
         STATUS_STR(CL_DEVICE_NOT_FOUND);
         STATUS_STR(CL_INVALID_BINARY);
         STATUS_STR(CL_INVALID_BUILD_OPTIONS);
@@ -356,50 +357,14 @@ string cl::format_status(cl_int e) {
 
     default:
         stringstream s;
-        s << e;
+        s << "Unknown";
         return s.str();
     }
 }
 
 void cl::handle_error(const char *code, cl_int e)
 {
-#define HANDLE(x)                                                       \
-    if(e == x) {                                                        \
-        cerr << code << " failed with error "                           \
-             << format_status(e) << " (" << e << ")" << endl;           \
-		abort();														\
-    }
- 
-    HANDLE(CL_BUILD_PROGRAM_FAILURE);
-    HANDLE(CL_COMPILER_NOT_AVAILABLE);
-    HANDLE(CL_DEVICE_NOT_FOUND);
-    HANDLE(CL_INVALID_BINARY);
-    HANDLE(CL_INVALID_BUILD_OPTIONS);
-    HANDLE(CL_INVALID_COMMAND_QUEUE);
-    HANDLE(CL_INVALID_CONTEXT);
-    HANDLE(CL_INVALID_DEVICE);
-    HANDLE(CL_INVALID_DEVICE_TYPE);
-    HANDLE(CL_INVALID_EVENT_WAIT_LIST);
-    HANDLE(CL_INVALID_GLOBAL_OFFSET);
-    HANDLE(CL_INVALID_GLOBAL_WORK_SIZE);
-    HANDLE(CL_INVALID_IMAGE_SIZE);
-    HANDLE(CL_INVALID_MEM_OBJECT);
-    HANDLE(CL_INVALID_KERNEL);
-    HANDLE(CL_INVALID_KERNEL_ARGS);
-    HANDLE(CL_INVALID_OPERATION);
-    HANDLE(CL_INVALID_PLATFORM);
-    HANDLE(CL_INVALID_PROGRAM);
-    HANDLE(CL_INVALID_PROGRAM_EXECUTABLE);
-    HANDLE(CL_INVALID_QUEUE_PROPERTIES);
-    HANDLE(CL_INVALID_VALUE);
-    HANDLE(CL_INVALID_WORK_DIMENSION);
-    HANDLE(CL_INVALID_WORK_GROUP_SIZE);
-    HANDLE(CL_INVALID_WORK_ITEM_SIZE);
-    HANDLE(CL_MEM_OBJECT_ALLOCATION_FAILURE);
-    //HANDLE(CL_MISALIGNED_SUB_BUFFER_OFFSET);
-    HANDLE(CL_OUT_OF_RESOURCES);
-    HANDLE(CL_OUT_OF_HOST_MEMORY);
-
-    cerr << code << " failed with unknown error (" << e << ")" << endl;
-    abort();
+    cerr << code << " failed with error "                           
+         << format_status(e) << " (" << e << ")" << endl;           
+	abort();														
 }
