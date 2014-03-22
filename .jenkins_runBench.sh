be9e8cd1a61d8889dd564528d79cd4378a3f6f26
Ryan Newton <rrnewton@gmail.com>
2014-03-22 15:07:43 -0400

    Switch to table id

--- .jenkins_runBench.sh
+++ .jenkins_runBench.sh
@@ -44,8 +44,13 @@ CID=905767673358.apps.googleusercontent.com
 SEC=2a2H57dBggubW1_rqglC7jtK
 
 # Harlan doc ID:  
-TABID=
+# Oops, I made one table as rrnewton:
+#   TABID=1h2w9Y41hbiDqCTWj3EhFRZbURVWo71Iw2ZbUw8Om
+# Here's the one owned by parfunc:
+TABID=1laMlzZ-qTCIqJake4ootb-B-ObRVITirmBkeEH3k
+# https://www.google.com/fusiontables/data?docid=1laMlzZ-qTCIqJake4ootb-B-ObRVITirmBkeEH3k
+
 
 # Enable upload of benchmarking data to a Google Fusion Table:
-# ./run_benchmarks.exe --keepgoing --trials=$TRIALS --fusion-upload=$TABID --clientid=$CID --clientsecret=$SEC $*
-./run_benchmarks.exe --keepgoing --trials=$TRIALS --fusion-upload --name=Harlan_bench_data --clientid=$CID --clientsecret=$SEC $*
+./run_benchmarks.exe --keepgoing --trials=$TRIALS --fusion-upload=$TABID --clientid=$CID --clientsecret=$SEC $*
+# ./run_benchmarks.exe --keepgoing --trials=$TRIALS --fusion-upload --name=Harlan_bench_data --clientid=$CID --clientsecret=$SEC $*
