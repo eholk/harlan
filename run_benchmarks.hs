2ad26f5bfa28da4b456c3547f7f08f500d490af3
Ryan Newton <rrnewton@gmail.com>
2014-03-22 16:35:18 -0400

    Do things the simple way for test bench-add

--- run_benchmarks.hs
+++ run_benchmarks.hs
@@ -34,7 +34,8 @@ main = defaultMainModifyConfig myconf
 
 all_benchmarks :: [Benchmark DefaultParamMeaning]
 all_benchmarks =
-  [ mkBenchmark "test/bench-add-vector.kfc" [] defaultCfgSpc
+  [ mkBenchmark "test/bench-add-vector.kfc" [show sz] defaultCfgSpc
+  | sz <- [1,3 .. 90]
   ]
 
 -- Default configuration space over which to vary settings:
